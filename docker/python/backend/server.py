8a34b71ae0b8f0941a1ab66b1be6d714dbb8a96a
Pellegrin69 <pellegrinpierre@hotmail.fr>
2023-12-06 11:12:53 +0100

    route get, add, health

--- docker/python/backend/server.py
+++ docker/python/backend/server.py
@@ -1,54 +1,63 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse, parse_qs
 import json
+
 
 class SimpleRequestHandler(BaseHTTPRequestHandler):
     data_store = []
+    iterator = 0
 
     def do_GET(self):
-        # Gérer la route /get
-        if self.path == '/get':
+        if self.path == '/health':
+            # Route /health
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-
-            # Renvoyer les données stockées sous forme de JSON
-            response_data = {'data': self.data_store}
+            response_data = {'status': 'OK'}
+            self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        elif self.path == '/get':
+            # Route /get
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            response_data = {'status': 'OK', 'iterator': self.iterator}
             self.wfile.write(json.dumps(response_data).encode('utf-8'))
         else:
-            # Si la route n'est pas reconnue, renvoyer une réponse 404
+            # Route non reconnue
             self.send_response(404)
             self.end_headers()
 
     def do_POST(self):
-        # Gérer la route /add
         if self.path == '/add':
-            # Récupérer la longueur du contenu du message
+            # Route /add
             content_length = int(self.headers['Content-Length'])
-            # Lire le corps du message POST
             post_data = self.rfile.read(content_length)
-            # Parser les données JSON
             data = json.loads(post_data.decode('utf-8'))
 
-            # Ajouter les données à la mémoire
-            self.data_store.append(data)
-
-            # Renvoyer une réponse 200 OK
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            response_data = {'message': 'Données ajoutées avec succès'}
-            self.wfile.write(json.dumps(response_data).encode('utf-8'))
+            # Vérifier que la clé 'value' est présente
+            if 'value' in data and isinstance(data['value'], int):
+                # Mettre à jour l'itérateur
+                self.iterator += data['value']
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                response_data = {'status': 'OK', 'message': 'Iterator value updated successfully'}
+                self.wfile.write(json.dumps(response_data).encode('utf-8'))
+            else:
+                # Erreur si la clé 'value' n'est pas présente ou n'est pas un entier
+                self.send_response(400)
+                self.end_headers()
         else:
-            # Si la route n'est pas reconnue, renvoyer une réponse 404
+            # Route non reconnue
             self.send_response(404)
             self.end_headers()
+
 
 def run_server(port=8000):
     server_address = ('', port)
     httpd = HTTPServer(server_address, SimpleRequestHandler)
     print(f'Starting server on port {port}...')
     httpd.serve_forever()
+
 
 if __name__ == '__main__':
     run_server()
