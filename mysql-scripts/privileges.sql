5fc471aab4205e634a49e69a400fcac4ed9604ec
Anacoundaa1 <momo2706@hotmail.fr>
2023-12-06 12:22:25 +0100

    Merge

--- mysql-scripts/privileges.sql
+++
@@ -1,8 +0,0 @@
-#-----------------------------------#
-#       USER RIGHTS MANAGEMENT      #
-#-----------------------------------#
-CREATE USER 'userIterator'@'%' IDENTIFIED BY 'qwerty1234';
-
-GRANT ALL PRIVILEGES ON `iterator-db`.* TO 'userIterator'@'%' WITH GRANT OPTION;
-
-FLUSH PRIVILEGES;
